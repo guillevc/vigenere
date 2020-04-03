81b0dc0ff784e9d8c77c9af6eeb8a158591685a5
Guillermo Alfonso Varela Chouciño <guillermo.vchoucino@udc.es>
2020-04-03 21:03:15 +0200

    update

--- vigenere.py
+++ vigenere.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
-dictionary_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326\JdP_001_dictionary')
+dictionary_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_dictionary')
+hash_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_hash')
+input_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_input')
 
-with open(dictionary_path) as f:
-    for char in f.read():
-        if char == '\n':
-            break
-        
+dictionary_str = next(open(dictionary_path)).rstrip()
+dictionary = [c for c in dictionary_str]
+
+def encrypt(plaintext, key, dictionary):
+    dictionary_len = len(dictionary)
+    key_length = len(key)
+    key_as_int = [dictionary.index(c) for c in key]
+    plaintext_int = [dictionary.index(c) for c in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % dictionary_len
+        ciphertext += dictionary[value]
+    return ciphertext
+
+def decrypt(ciphertext, key, dictionary):
+    dictionary_len = len(dictionary)
+    key_length = len(key)
+    key_as_int = [dictionary.index(c) for c in key]
+    ciphertext_int = [dictionary.index(c) for c in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % dictionary_len
+        plaintext += dictionary[value]
+    return plaintext
