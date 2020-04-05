120cce8c377cccebeeda6abf87d0d595074b03d4
Guillermo Alfonso Varela Chouciño <guillermo.vchoucino@udc.es>
2020-04-05 02:56:41 +0200

    refactor

--- vigenere.py
+++ vigenere.py
@@ -1,43 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Basic implementation from https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4
-
 import os
 import sys
 import argparse
 
-dictionary_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_dictionary')
-hash_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_hash')
-input_path = os.path.join(os.path.dirname(__file__), 'JdP_vigenere_alumnos_20200326', 'JdP_001_input')
-
-dictionary_str = next(open(dictionary_path)).rstrip()
-dictionary = [c for c in dictionary_str]
-
-def encrypt(plaintext, key, dictionary):
-    dictionary_len = len(dictionary)
-    key_length = len(key)
-    key_as_int = [dictionary.index(c) for c in key]
-    plaintext_int = [dictionary.index(c) for c in plaintext]
-    ciphertext = ''
-    for i in range(len(plaintext_int)):
-        value = (plaintext_int[i] + key_as_int[i % key_length]) % dictionary_len
-        ciphertext += dictionary[value]
-    return ciphertext
-
-def decrypt(ciphertext, key, dictionary):
-    dictionary_len = len(dictionary)
-    key_length = len(key)
-    key_as_int = [dictionary.index(c) for c in key]
-    ciphertext_int = [dictionary.index(c) for c in ciphertext]
-    plaintext = ''
-    for i in range(len(ciphertext_int)):
-        value = (ciphertext_int[i] - key_as_int[i % key_length]) % dictionary_len
-        plaintext += dictionary[value]
-    return plaintext
-
-
-if __name__ == "__main__":
+def main():  
     parser = argparse.ArgumentParser(description='Algorithm for cypher and decypher texts.', 
         add_help=True)
     parser.add_argument('-i', metavar='INPUT', nargs='?', type=argparse.FileType(), 
@@ -61,3 +29,30 @@ if __name__ == "__main__":
     proccessed_text = args.i.read()
     dictionary_text = args.d.read()
     hash_text = args.hash.read()
+
+# Source: https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4
+def encrypt(plaintext, key, dictionary):
+    dictionary_length = len(dictionary)
+    key_length = len(key)
+    key_indexes = [dictionary.index(c) for c in key]
+    plaintext_indexes = [dictionary.index(c) for c in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_indexes)):
+        value = (plaintext_indexes[i] + key_indexes[i % key_length]) % dictionary_length
+        ciphertext += dictionary[value]
+    return ciphertext
+
+def decrypt(ciphertext, key, dictionary):
+    dictionary_length = len(dictionary)
+    key_length = len(key)
+    key_indexes = [dictionary.index(c) for c in key]
+    ciphertext_indexes = [dictionary.index(c) for c in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_indexes)):
+        value = (ciphertext_indexes[i] - key_indexes[i % key_length]) % dictionary_length
+        plaintext += dictionary[value]
+    return plaintext
+
+
+if __name__ == "__main__":
+    main()
