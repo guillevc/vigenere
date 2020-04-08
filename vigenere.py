b1c5e3f5715e00f49d944e3d24f2a0001207d6f7
Guillermo Alfonso Varela Chouciño <guillermo.vchoucino@udc.es>
2020-04-08 22:39:02 +0200

    update

--- vigenere.py
+++ vigenere.py
@@ -8,6 +8,7 @@ import itertools
 import hashlib
 import time
 import logging
+import collections
 
 MAX_KEY_LENGTH = 10
 
@@ -26,38 +27,84 @@ def main():
     dictionary_text = args.dictionary.read().strip()
     hash_text = args.hash.read().strip()
 
-    # proccessed_text = 'EYKANGBAFUMMAQCZAJJUNIJNTXUTOJYCORKRAJYMAZYRIGFSASCTAXCOAIBINGKARGBACKMFRKHTEGFCOXJNABCRUYKORRCLLUHESJYEUXJSSKAUNNUEXVFICGXOEYÑEMÑYRCUFESKFMISCSTXJDEYUNIJUDSGFVAJJRIQFASKÑRAZUDERUSCGMILQUSGAUNTKNYMGÑERÑULDKKROZYCCÑJNTKNTRGKIDUNYEWOIPUNDEXYSPÑMACÑJNAYCSTÑXAIRKREYWINJCBLKNPAXULOYKACÑYNTKNMAYARABYSCUHCOBCDEYÑASKGANGÑENJMEMUNUNGKRIRYRAKHTRKAADKGATKMIAQYSHGUVASTADUBEMUNCORKRAJJYPGAADUGILQJNEYXEMGNCAXCLLGNQUKYMPKTARGHALQYGAXSAYYYGUÑMANQFEGGHDOJORASÑELGNPRURIMGNOCNJSERUNAYFLEMURASGILQJNEYXEGAUNTKNDUXUNTKFASVMOXÑGASICNCUNEMGHASKHCUGHTOGÑESZMAPÑXOSNYMOYUDQACRIJJMIQFONKNPAXULOYGESKNDERURZUSABXCLAJYMAYMECÑVIRKGOSKLUIVJSDKMESVCRAICONGNISZCDAJORASÑELUNMEYYSDKUBRÑFAJAHIONUEXVFICGXOIQFAEYÑAMUNGEYÑIOSUNDUFACUGPRGXEMGNEQACPOYBAASUDIJJELXYSPUHSAHFEDKNANÑXADNUREIJRDGXOQAYELMJBIKMNONUREVURTÑXOCGNISKCSMÑFLOSYSDKGASIURIQFASAHASICFRGNQUKXESMMANGMAEQDUEBYSESYLCUHGRKNOAJYMAYYLEOYCUZCVOKNTAGWTIBUNDUFAFGVRIIUCIUHNAICONGFDEZJDAYYSTGNCAZYGOXCASJYPRUXUCZJSRKNPIXUDOXYSMGÑERÑULDKKROZYCCÑJNYZYSTXUPIJJSEQGINÑNTRUBAAMMADKWIDUYLCUGPRUGISUXELGNEMVMESGNESVUNOQUSPGMAAJUPTGMSUYFINKUSDKKROJOCCÑJNAQUFAHMICGWIOSXEEYÑOSRUTEXCALKNYANUYDUNEMVMESGNQUKZABXCCASMESVCRAJJREYYNEYKANG'
-    # dictionary_text = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
-    # hash_text = '5442d541845e30ef6af885af537d41d35b2fa5e21fb47a5eae98043c441362e1'
+    # proccessed_text = 'CUMDILHUQPCRLNXAYNQGZWQZCYECILNLXANNGCQZFNRSSSUSEBQPUSPHREIOFVKTMGEBDGCBECLDXZEUNCJDCJDEOZWFXMFNNEZNWDDMSZCSFHKPMRLLZYMNXCRAUHOQGTFDDILPMHXJKPLZYMNXQZCYLLSKZWJEBDDSREYLTHZHUXHBHNBHDHNEXHQZHNOKEZNCUJYIVWYCRYZMFDAYQDIMEIQPPDCMDXIQPUCGUMNYCXUKHUQPORPMZEYBSHHBODNUKWYCNLXANNGCQLFDINNCNHZHHYQGTWGTNDYWQJJSDNGPPHNNHXMETFDDGZVCMRNGPGHYUBNYRDCAWYZYXCPGZYXRLLZYMNXJZJGDYNSZXDNLXANSSYLTHZALNAYQWSHXJKPGDYNDOWQJJSZPHCUKPRSZLSTIMLNSLWJCYBZPDCCMRNGPZHWYRHCSSITENGPXDNLXANHZHJPSHDUMTHSCUBEUAWYOCIAWYLLHCOCEQCBFFSEISCUBPXHRCSLFBFLQPHBTYRDOBSURFEZDBNCVHEWNTHZYXNEBDCWQJJSZWTCLDYWXLLDFMDOZNCNGPLZYMNXMLLEHYASCUBTHFLHCALNDYBFNHYASSYOPLOPNQLNNCMCTZETWTWNQLHRZGVLLDLNSLWJDUQPNXACBLFKJWZCLHPXNFNTDCMRUSCIILHSSUSTMCTMFFCRPXZDUKPAHECLLNDQCKPNGLNSSYTDYQTMSCCBVYCTHSZXNHHKZUCTHFZLNAYMTHFHBDYCSLLQTPDDURLHDXUHWUSEUBSGDYNGZQDGYQZHDSCFSJQZZHWYDIULAFDEBDHUMYUBCSVZLLELZGYKWYCLOSZGZECBLFKJVDEQDPHBZGOFNDCMVTNGZOSFMDCCMEYQLWSTIMDNZCNHYAECILLLNFHCEBDFMDZZQLHRZGVLLDDWZXMGLMFCIVYCMEYQYUSTIMLFKJNGPLDHYQPGHWFHZHQLHRZGVLLDLNSLWJDCMEBDQCQDNRTRLZHSSMNQNGTMLLLJDUHYWQPURPIUPLSSCRDULPNHXYECULPCMTHIFHDGYMOIQXWZQYDCYKPURPXCLNZDBNHCMRNGLNHEBZOWNWFDNNDOGNCYSSUMOITMFDEBDYOLMYQZZRLGOWYRZZQLHRZGVLLDEBZEKTLLSPLSSUMTNGLXHYNGPMZXYPFUQEYQZZSSYOCYUTITDSDLLBCSOEIKZWJPLVLMOLLSTWTWUQWSRFWBPMRQOKALNNOQTHFLHDDNHXUSPXTDGHWFHZHAPZNCYHEQZDNZVYMOIVYVXLOSSIQTNHPMZYXBCSOEIVLFKHURPMSTGZEYCMSSSYTDZDOYQLFAFLDLONQCMGYRECFLNHZHEMCSZBZGYZNWQFYCZPDCORXCKWCNYVXUOMP'
+    # dictionary_text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # hash_text = '00c6d6db4c17e3bf8d8cbf972ad5a3bd417eff5d120ca65ddf60fa9b7b33463d'
+
+    dictionary_char_to_index = {char: index for index, char in enumerate(dictionary_text)}
+    dictionary_index_to_char = {index: char for index, char in enumerate(dictionary_text)}
 
     start_time = time.time()
-    key = guess_key(proccessed_text, dictionary_text, hash_text)
+    key = guess_key(proccessed_text, dictionary_text, dictionary_char_to_index, dictionary_index_to_char, hash_text)
     print('Successfully guessed key {} in {} seconds'.format(key, time.time() - start_time))
 
-def guess_key(ciphertext, dictionary, hash_text):
+def guess_key(ciphertext, dictionary, dictionary_char_to_index, dictionary_index_to_char, hash_text):
     key_lenghts = get_key_lengths(ciphertext)
+    print(key_lenghts)
 
     for key_length in key_lenghts:
+        if (key_length != 6):
+            continue
         print('Attempting with key length {}...'.format(key_length))
-        key = guess_key_attempt_with_key_length(ciphertext, key_length, dictionary, hash_text)
+        key = guess_key_attempt_with_key_length(ciphertext, key_length, dictionary_char_to_index, dictionary_index_to_char, hash_text)
         if (key != None):
             return key
     return None
 
-def guess_key_attempt_with_key_length(ciphertext, most_likely_key_length, dictionary, hash_text):
-    for combination in itertools.product(dictionary, repeat=most_likely_key_length):
+def guess_key_attempt_with_key_length(ciphertext, key_length, dictionary_char_to_index, dictionary_index_to_char, hash_text):
+    # for combination in itertools.product(dictionary, repeat=key_length):
+    #     key = ''.join(combination)
+    #     #print('Testing with key {}'.format(key))
+    #     decrypted_text = decrypt(ciphertext, key, dictionary)
+    #     hashed_message = hashlib.sha256(decrypted_text.encode('utf-8')).hexdigest()
+    #     if (hashed_message == hash_text):
+    #         return key
+
+    #print(most_common_characters(ciphertext, 10))
+    probable_characters = []
+    for i in range(key_length):
+        ciphertext_sample = ciphertext
+        col = ciphertext_sample[i::key_length + i]
+        most_common_character = most_common_characters(col, 1)[0][0]
+        print(i, most_common_characters(col, 5))
+        probable_characters.append(guess_probable_characters(most_common_character, key_length, dictionary_char_to_index, dictionary_index_to_char))
+    print(probable_characters)
+
+    for combination in itertools.product(*probable_characters):
         key = ''.join(combination)
-        #print('Testing with key {}'.format(key))
-        decrypted_text = decrypt(ciphertext, key, dictionary)
+        print(key)
+        decrypted_text = decrypt(ciphertext, key, dictionary_char_to_index, dictionary_index_to_char)
         hashed_message = hashlib.sha256(decrypted_text.encode('utf-8')).hexdigest()
         if (hashed_message == hash_text):
             return key
+
+MOST_COMMON_SPANISH = ['E', 'A', 'O', 'S', 'R', 'N', 'I', 'D', 'L', 'C', 'T', 'U', 'M', 'P', 'B', 'G', 'V', 'Y', 'Q', 'H', 'F', 'Z', 'J', 'Ñ', 'X', 'K', 'W']
+MOST_COMMON_ENGLISH = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D', 'L', 'U', 'W', 'M', 'F', 'C', 'G', 'Y', 'P', 'B', 'K', 'V', 'J', 'X', 'Q', 'Z']
+
+def guess_probable_characters(character, key_length, dictionary_char_to_index, dictionary_index_to_char):
+    dictionary_chars = dictionary_char_to_index.keys()
+    dictionary_length = len(dictionary_chars)
+    if 'Ñ' in dictionary_chars:
+        MOST_COMMON = MOST_COMMON_SPANISH
+    else:
+        MOST_COMMON = MOST_COMMON_ENGLISH
+    most_common_indexes = [dictionary_char_to_index.get(c) for c in MOST_COMMON]
+    character_index = dictionary_char_to_index.get(character)
+    probable_characters = []
+    for i in most_common_indexes:
+        j = (character_index - i) % dictionary_length
+        probable_characters.append(dictionary_index_to_char.get(j))
+    return probable_characters
+
+def most_common_characters(text, count):
+    return collections.Counter(text).most_common(count)
 
 def get_key_lengths(ciphertext):
     # Find out the sequences of 3 to 6 letters that occur multiple times
     # in the ciphertext. repeated_sequences_spacings has a value like:
     # {'EXG': [192], 'NAF': [339, 972, 633], ... }
-    repeated_sequences_spacings = get_repeated_sequences_with_spacings(ciphertext, 3, 6)
+    ciphertext_sample = ciphertext[:5000]
+    repeated_sequences_spacings = get_repeated_sequences_with_spacings(ciphertext_sample, 3, 6)
 
     # Get factors for each sequence spacing
     sequence_factors = {}
@@ -115,26 +162,26 @@ def get_factors(num):
     return list(set(factors))
 
 # Source: https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4
-def encrypt(plaintext, key, dictionary):
-    dictionary_length = len(dictionary)
+def encrypt(plaintext, key, dictionary_char_to_index, dictionary_index_to_char):
+    dictionary_length = len(dictionary_char_to_index.keys())
     key_length = len(key)
-    key_indexes = [dictionary.index(c) for c in key]
-    plaintext_indexes = [dictionary.index(c) for c in plaintext]
+    key_indexes = [dictionary_char_to_index.get(c) for c in key]
+    plaintext_indexes = [dictionary_char_to_index.get(c) for c in plaintext]
     ciphertext = ''
     for i in range(len(plaintext_indexes)):
         value = (plaintext_indexes[i] + key_indexes[i % key_length]) % dictionary_length
-        ciphertext += dictionary[value]
+        ciphertext += dictionary_index_to_char.get(value)
     return ciphertext
 
-def decrypt(ciphertext, key, dictionary):
-    dictionary_length = len(dictionary)
+def decrypt(ciphertext, key, dictionary_char_to_index, dictionary_index_to_char):
+    dictionary_length = len(dictionary_char_to_index.keys())
     key_length = len(key)
-    key_indexes = [dictionary.index(c) for c in key]
-    ciphertext_indexes = [dictionary.index(c) for c in ciphertext]
+    key_indexes = [dictionary_char_to_index.get(c) for c in key]
+    ciphertext_indexes = [dictionary_char_to_index.get(c) for c in ciphertext]
     plaintext = ''
     for i in range(len(ciphertext_indexes)):
         value = (ciphertext_indexes[i] - key_indexes[i % key_length]) % dictionary_length
-        plaintext += dictionary[value]
+        plaintext += dictionary_index_to_char.get(value)
     return plaintext
 
 if __name__ == '__main__':
