4637e6c5b0d5a25be3272772fce4702c304564c0
Guillermo Alfonso Varela Chouciño <guillermo.vchoucino@udc.es>
2020-04-14 18:30:30 +0200

    passing arguments in script

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_001_input -d ./JdP_vigenere_alumnos_20200326/JdP_001_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_001_hash --max-key-length 6
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_002_input -d ./JdP_vigenere_alumnos_20200326/JdP_002_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_002_hash --max-key-length 6
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_003_input -d ./JdP_vigenere_alumnos_20200326/JdP_003_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_003_hash --max-key-length 6
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_004_input -d ./JdP_vigenere_alumnos_20200326/JdP_004_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_004_hash --max-key-length 6
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_005_input -d ./JdP_vigenere_alumnos_20200326/JdP_005_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_005_hash --max-key-length 6
-python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_006_input -d ./JdP_vigenere_alumnos_20200326/JdP_006_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_006_hash --max-key-length 6
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_001_input -d ./JdP_vigenere_alumnos_20200326/JdP_001_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_001_hash --max-key-length 6 "$@"
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_002_input -d ./JdP_vigenere_alumnos_20200326/JdP_002_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_002_hash --max-key-length 6 "$@"
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_003_input -d ./JdP_vigenere_alumnos_20200326/JdP_003_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_003_hash --max-key-length 6 "$@"
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_004_input -d ./JdP_vigenere_alumnos_20200326/JdP_004_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_004_hash --max-key-length 6 "$@"
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_005_input -d ./JdP_vigenere_alumnos_20200326/JdP_005_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_005_hash --max-key-length 6 "$@"
+python3 vigenere.py -i ./JdP_vigenere_alumnos_20200326/JdP_006_input -d ./JdP_vigenere_alumnos_20200326/JdP_006_dictionary --hash JdP_vigenere_alumnos_20200326/JdP_006_hash --max-key-length 6 "$@"
